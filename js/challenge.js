b6021c8a736a6bf746e6378f484d17623ac8e45a
Julien Hnery <jwh_917@yahoo.com>
2022-02-28 15:47:22 -0500

    Completed Assignment

--- js/challenge.js
+++ js/challenge.js
@@ -16,58 +16,131 @@ const minusButton = document.getElementById("minus")
 
 minusButton.addEventListener('click', (event) => {
     count = count - 1
-    counterId.textContent = count
+    counterId.innerText = count
 });
 
 const plusButton = document.getElementById("plus")
 
 plusButton.addEventListener('click', (event) => {
         count = count + 1
-        counterId.textContent = count
+        counterId.innerText = count
 });
 
 const likeButton = document.getElementById("heart")
 
+let x = 0 
 
 likeButton.addEventListener('click', (event) => {
-    let x = 0 
-    //x ++
     const listLikes = document.querySelector(".likes")
     const li = document.createElement("li")
     let screenNum = parseInt(counterId.innerText)
-    
     let key = count
-    //setOfLikes[key] = x+=1
     
-    if (setOfLikes[key] === setOfLikes[key]) {
-        setOfLikes[key] = x+=1
-        const textNode = document.createTextNode(screenNum + " has been liked " + x + " times")
-        li.appendChild(textNode)
-        listLikes.appendChild(li)
-        console.log(x)
+    if (document.getElementById(count)) {
+        //console.log("found",document.getElementById(count))
+        setOfLikes[key] += 1
+        let found = document.getElementById(count)
+        found.innerHTML = screenNum + " has been liked " + setOfLikes[key] + " times"
+        //console.log(setOfLikes)
     } else {
-        const textNode = document.createTextNode(screenNum + " has been liked  1 time")
-        li.appendChild(textNode)
+        li.id = count
+        //console.log(li)
+        li.innerHTML = screenNum + " has been liked  1 time"
         listLikes.appendChild(li)
-        console.log(x)
+        setOfLikes[key] = 1
+        //console.log("else", setOfLikes)
     }
 })
 
 const pauseButton = document.getElementById("pause")
+const submitButton = document.getElementById("submit")
+//const resumeButton = document.getElementById("resume")
+
 
 pauseButton.addEventListener('click', (event) => {
     clearInterval(intervalId)
-    minusButton.disabled
-    plusButton.disabled
-    likeButton.disabled
-    submitButton.disabled
+    minusButton.disabled = true
+    plusButton.disabled = true
+    likeButton.disabled = true
+    submitButton.disabled = true  
+    pauseButton.id = "resume"
+    pauseButton.innerHTML = "resume"
+
+    //maybe if 
+    // setInterval( function(){
+    //     count += 1
+    //     counterId.innerText = count
+    // }, 1000)
+    // minusButton.disabled = false
+    // plusButton.disabled = false
+    // likeButton.disabled = false
+    // submitButton.disabled = false
+    // pauseButton.id = "pause"
+    // pauseButton.innerHTML = "pause"
     
-
-
 
 })
 
-const submitButton = document.getElementById("submit")
+
+// resumeButton.addEventListener('click', (event) => {
+//     setInterval( function(){
+//         count += 1
+//         counterId.innerText = count
+//     }, 1000)
+//     minusButton.disabled = false
+//     plusButton.disabled = false
+//     likeButton.disabled = false
+//     submitButton.disabled = false
+//     resumeButton.id = "pause"
+//     pauseButton.innerHTML = "pause"
+    
+
+// })
+
+const restartButton = document.getElementById("restart")
+
+// repeated click makes it go faster ?
+restartButton.addEventListener('click', (event) => {
+    count = -1
+    clearInterval(intervalId)
+    setInterval( function(){
+        count += 1
+        counterId.innerText = count
+    }, 1000)
+    minusButton.disabled = false
+    plusButton.disabled = false
+    likeButton.disabled = false
+    submitButton.disabled = false
+    
+})
+
+//how to get extra space ?
+
+submitButton.addEventListener('click', (event) => {
+    event.preventDefault()
+    let commentsArea = document.querySelector(".comments")
+    let commentBoxValue = document.getElementById("comment-input").value
+    let pageComments = document.createTextNode(commentBoxValue)
+    let br = document.createElement("br")
+    if(document.getElementById("comment-input").value !== " "){
+        commentsArea.appendChild(pageComments)
+        commentsArea.appendChild(br)
+        commentsArea.appendChild(br);
+        document.getElementById("comment-input").value = " "
+    }
+    else{
+
+    }
+
+})
+
+
+
+
+
+
+
+
 
 
 
